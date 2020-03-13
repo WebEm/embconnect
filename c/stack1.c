bc5544d31da53cc4e33fb667b4eb4a0e0dfba008
bhuvaneswari <bhuvaneswarichinnu12@gmail.com>
2020-03-13 14:54:09 +0530

    my first stack program

--- c/stack1.c
+++ c/stack1.c
@@ -13,7 +13,9 @@ void display();
 void stack_count();
 void destroy();
 void create();
+
 int count = 0;
+
 void main()
 {
 	int no,ch,e;
@@ -25,68 +27,68 @@ void main()
     	printf("\n 6 - display");
         printf("\n 7 - stack count");
         printf("\n 8 - destroy");
+
 	create();
-	while(1){
+
+	while (1) {
 		printf("\n enter choice:");
 		scanf("%d",&ch);
 
-		switch(ch)
-		{
+		switch (ch) {
 		case 1:
-		       printf("enter data:");
-	       	       scanf("%d",&no);
-		       push(no);
-		       break;
+		    printf("enter data:");
+	       	    scanf("%d",&no);
+		    push(no);
+	            break;
 		case 2:
-		       pop();
-		       break;
+	            pop();
+	            break;
 		case 3:
-		       if(top == NULL)
-			      printf("no elements in stack");
-		       else
-		       {
-			      e = topelement();
-		              printf("\n top element :%d",e);
-		       }	      
-		       break;
-		case 4:
-		       empty();
-                       break;
+		    if (top == NULL)
+			    printf("no elements in stack");
+		    else {
+		            e = topelement();
+		            printf("\n top element :%d",e);
+		    }	      
+		    break;
+ 		case 4:
+	            empty();
+                    break;
                 case 5:
-                       exit(0);
+                    exit(0);
 	        case 6:
-                       display();
-                       break;
+                    display();
+                    break;
                 case 7:
-                       stack_count();
-		       break;
+                    stack_count();
+                    break;
 		case 8:
-		       destroy();
-		       break;
+       	            destroy();
+	            break;
 		default:
-		       printf("wrong choice,please enter the correct choice");
-		       break;
+		    printf("wrong choice,please enter the correct choice");
+                    break;
 		}
-	}
+        } 
 }
+
 void create()
 {
 	top = NULL;
 }
+
 void stack_count()
 {
 	printf("no.of elements in stack : %d",count);
 }
+
 void push(int data)
 {
-	if (top = NULL)
-	{
+	if (top = NULL) {
 		top=(struct node*)malloc(1*sizeof(struct node));
 		top->p = NULL;
 		top->info = data;
-	}
-        else
-	{
+	} else {
 	        temp=(struct node*)malloc(1*sizeof(struct node));
                 temp->p = NULL;
                 temp->info = data;
@@ -94,36 +96,38 @@ void push(int data)
 	}
         count++;
 }
+
 void display()
 {
 	top1 = top;
-        if (top1 == NULL){
+        if (top1 == NULL) {
                 printf("stack is empty");
                 return;
-        }
-        while(top1 != NULL){
+        } while (top1 != NULL) {
 		printf("%d",top1->info);
 		top1 = top1->p;
 	}
 }
+
 void pop()
 {
         top1 = top;
-        if(top1 == NULL){
+        if (top1 == NULL) {
 	        printf("\n trying to pop from empty stack");
 	        return;
-	}
-        else
+	} else
 	        top1 = top1->p;
         	printf("\n popped value :%d",top->info);
 		free(top);
 		top = top1;
 		count--;
 }
+
 int topelement()
 {
 	return(top->info);
 }
+
 void empty()
 {
 	if (top == NULL)
@@ -131,10 +135,11 @@ void empty()
         else
 		 printf("\n stack is not empty with %d elements",count);
 }
+
 void destroy()
 {
 	top1 = top;
-	while (top1 !=NULL){
+	while (top1 !=NULL) {
 		top1 = top->p;
 		free(top);
 		top = top1;
@@ -142,6 +147,7 @@ void destroy()
 	}
 	free(top1);
 	top = NULL;
+
 	printf("\n all stack elements destroyed");
 	count = 0;
 }
