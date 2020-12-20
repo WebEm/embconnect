324d3d069b312fe6bc5bc972fa54c7b224bbc6f8
gayathri-bytes <73304225+gayathri-bytes@users.noreply.github.com>
2020-12-20 16:41:10 +0530

    Update thread.c

--- thread.c
+++ thread.c
@@ -3,13 +3,13 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
 
-2020+ Copyright (c)
+2020+ Copyright (c) gayathri-bytes <gaya3velu.25@gmail.com>
 All rights reserved.
 */
 
-#include<stdio.h>
-#include<pthread.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 void *thread_function()
 {
@@ -19,8 +19,11 @@ void *thread_function()
 
 void main()
 {
+        int return;
         pthread_t thread;
         printf("First thread is created\n");
-        pthread_create(&thread, NULL, thread_function, NULL);
+        return = pthread_create(&thread, NULL, thread_function, NULL);
+        if (return != 0 )
+                printf("Thread couldn't be created %d", return);
         pthread_join(thread, NULL);
 }
